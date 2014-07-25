b985ea16bbd7f4049cfa1e7aa69904734019ef71
Valera19900 <kaktuz34@gmail.com>
2014-07-25 17:48:42 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -109,4 +109,46 @@ $(document).ready(function() {
 			if (element.attr("name") == "message") error.insertAfter($("input[name=message]"));
 		}
 	});
+	
+	//select
+	function select() {
+		var el = $('.js-select-other');
+		var el_title = el.children("span");
+		var item = el.find('li');
+		var input = el.find(".js-select-input");
+		var el_text = el.find(".js-select-text");
+		var checkbox = el.find(".checkbox");
+		var list = el.find('.js-select-drop');
+		el_title.bind("click",function(event){
+			// tooltip();
+			if ($(this).parent().hasClass('is-open')) {
+				$(this).parent().removeClass('is-open');
+				$(this).parent().parent().parent().removeClass('is-open');
+			}
+			else {
+				el.removeClass('is-open');
+				$('.row').removeClass('is-open')
+				$(this).parent().addClass('is-open');
+				$(this).parent().parent().parent().addClass('is-open');
+			};
+			event.stopPropagation();
+		});
+		checkbox.bind("click",function(event){
+			event.stopPropagation();
+		});
+		item.bind("click",function(){
+			$(this).toggleClass("is-selected");
+			var text = $(this).html();
+			var id = $(this).attr("data-id");
+			$(this).parents(".js-select-other").find(".js-select-text").html(text);
+			$(this).parents(".js-select-other").find(".js-select-input").val(id);
+	 });
+	};
+	select();
+
+	//click document
+	$(document).click(function() {
+		$('.js-select-other').removeClass('is-open');
+	});
+
 });
